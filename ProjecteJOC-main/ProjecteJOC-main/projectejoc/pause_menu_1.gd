d9778e9d03078485bb6e1c6ec0c9310d6ba04954
maxcartagenaa <159330354+maxcartagenaa@users.noreply.github.com>
2025-05-15 01:27:50 +0200

    pausa feta

--- ProjecteJOC-main/ProjecteJOC-main/projectejoc/pause_menu_1.gd
+++ ProjecteJOC-main/ProjecteJOC-main/projectejoc/pause_menu_1.gd
@@ -1,18 +1,26 @@
 extends Control
 
-var _is_paused:bool = false:
+var _is_paused := false
+
+var is_paused: bool:
 	set(value):
 		_is_paused = value
-		get_tree().paused = _is_paused
-		visible = _is_paused
-	
+		get_tree().paused = value
+		visible = value
+	get:
+		return _is_paused
+
+func _ready() -> void:
+	is_paused = false
+
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_cancel"):
-		_is_paused = !_is_paused
-
+		is_paused = !is_paused
 
 func _on_resume_button_pressed() -> void:
-	self.is_paused = false
+	is_paused = false
+	
 
 func _on_main_menu_button_pressed() -> void:
+	get_tree().paused = false  
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
