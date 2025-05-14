dfe3c925c2bdb30e6fb29a4e2c5896d9a98e1a81
mvalls80 <marcvalls08mvp@gmail.com>
2025-05-14 18:53:48 +0200

    13

--- ProjecteJOC-main/ProjecteJOC-main/projectejoc/scripts/nivell_1.gd
+++ ProjecteJOC-main/ProjecteJOC-main/projectejoc/scripts/nivell_1.gd
@@ -1,1 +1,7 @@
-extends Node2D
+extends Node
+
+const TARGET_SCENE_PATH := "res://scenes/pause_menu.tscn"
+
+func _process(delta):
+	if Input.is_action_just_pressed("ui_cancel"):
+		get_tree().change_scene_to_file(TARGET_SCENE_PATH)
