9971bc62950940b504951f604155fb5001beccb2
mvalls80 <marcvalls08mvp@gmail.com>
2025-05-15 20:55:17 +0200

    17

---
+++ ProjecteJOC-main/ProjecteJOC-main/projectejoc/scenes/spawner5.gd
@@ -0,0 +1,27 @@
+extends Area2D
+
+@onready var Enemy_scene = load("res://scenes/enemy.tscn")
+
+var bool_spawn = true
+
+var random = RandomNumberGenerator.new()
+
+func _ready() -> void:
+	random.randomize()
+	
+func _process(delta: float) -> void:
+	spawn()
+ 
+func spawn():
+	if bool_spawn:
+		$Timer.start()
+		bool_spawn = false
+		var enemy_instance = Enemy_scene.instantiate()
+		enemy_instance.position = Vector2(random.randi_range(1147.0,1748.0), random.randi_range(-66.0,409.0))
+		add_child(enemy_instance)
+
+
+
+
+func _on_timer_timeout() -> void:
+	bool_spawn = true
---
+++ ProjecteJOC-main/ProjecteJOC-main/projectejoc/scenes/spawner6.gd
@@ -0,0 +1,27 @@
+extends Area2D
+
+@onready var Enemy_scene = load("res://scenes/enemy.tscn")
+
+var bool_spawn = true
+
+var random = RandomNumberGenerator.new()
+
+func _ready() -> void:
+	random.randomize()
+	
+func _process(delta: float) -> void:
+	spawn()
+ 
+func spawn():
+	if bool_spawn:
+		$Timer.start()
+		bool_spawn = false
+		var enemy_instance = Enemy_scene.instantiate()
+		enemy_instance.position = Vector2(random.randi_range(1131.0,1766.0), random.randi_range(523.0,733.0))
+		add_child(enemy_instance)
+
+
+
+
+func _on_timer_timeout() -> void:
+	bool_spawn = true
