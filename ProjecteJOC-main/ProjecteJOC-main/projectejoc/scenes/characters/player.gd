bc70be26cc79e3d848ab2723a89440ca30b83ed8
maxcartagenaa <159330354+maxcartagenaa@users.noreply.github.com>
2025-05-15 22:58:04 +0200

    -

--- ProjecteJOC-main/ProjecteJOC-main/projectejoc/scenes/characters/player.gd
+++ ProjecteJOC-main/ProjecteJOC-main/projectejoc/scenes/characters/player.gd
@@ -4,4 +4,20 @@ extends CharacterBody2D
 
 var player_direction: Vector2
 
-var health = 50
+var max_health := 100
+var current_health := 100
+
+@onready var health_bar := $ProgressBar
+
+func take_damage(amount: int) -> void:
+	current_health -= amount
+	current_health = max(current_health, 0)
+	update_health_bar()
+	if current_health == 0:
+		die()
+
+func update_health_bar() -> void:
+	health_bar.value = current_health
+
+func die() -> void:
+	get_tree().change_scene_to_file("res://scenes/you_died_menu.tscn")
